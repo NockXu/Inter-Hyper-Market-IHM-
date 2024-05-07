c896794586434f63281e3583c191fceb1d972fa7
Aurélien FONTAINE <147625120+Aurelienxx@users.noreply.github.com>
2024-05-07 14:30:00 +0200

    Add files via upload

--- app2/vueClient.py
+++ app2/vueClient.py
@@ -2,11 +2,13 @@ import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QSizePolicy
 from PyQt6.QtWidgets import QLabel, QLineEdit, QPushButton, QMainWindow, QToolBar, QComboBox
 from PyQt6.QtCore import Qt, pyqtSignal
-from PyQt6.QtGui import QIcon, QAction, QPixmap, QGuiApplication
+from PyQt6.QtGui import QIcon, QAction, QPixmap, QGuiApplication, QFont
 
-# -----------------------------------------------------------------------------
-# --- class Interface
-# -----------------------------------------------------------------------------
+##########################################################
+#                                                        #
+#                    Classe vueClient                    #
+#                                                        #
+##########################################################
 class vueClient(QMainWindow):
     # constructeur
     def __init__(self):
@@ -20,20 +22,6 @@ class vueClient(QMainWindow):
 
         super().__init__()
 
-        # layout vertical --> principal layout
-        self.mainlayout = QVBoxLayout()
-        self.setLayout(self.mainlayout)
-        
-##########################################################
-#                                                        #
-#                     Central Widget                     #
-#                                                        #
-##########################################################
-
-        # Créer un Widget central pour y introduire un layout
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-
 ##########################################################
 #                                                        #
 #                        Layouts                         #
@@ -41,27 +29,20 @@ class vueClient(QMainWindow):
 ##########################################################
 
         # Layout vertical principal
-        self.layout_principal = QVBoxLayout()
+        self.layout_principal = QHBoxLayout()
         centre = QWidget()
         centre.setLayout(self.layout_principal)
         self.setCentralWidget(centre)
 
-        # Layout vertical pour le menu hamburger et la zone de texte
+        # Layout vertical pour le menu des magasins et de la liste
         self.menu_selection = QVBoxLayout()
         self.layout_principal.addLayout(self.menu_selection)
 
-        self.magasin = QComboBox()
-        self.magasin.addItem("Choix du magasin")
-        self.magasin.addItem("Magasin 1")
-        self.magasin.addItem("Magasin 2")
-        self.magasin.addItem("Magasin 3")
-        self.menu_selection.addWidget(self.magasin)
-
-        # Liste de course
-        self.liste = QTextEdit()
-        self.menu_selection.addWidget(self.liste)
-
-
+        # Layouts des filtres 
+        self.filtre1_layout = QVBoxLayout()
+        self.layout_principal.addLayout(self.filtre1_layout)
+        self.filtre2_layout = QHBoxLayout()
+        
 ##########################################################
 #                                                        #
 #                        Widgets                         #
@@ -77,20 +58,68 @@ class vueClient(QMainWindow):
         self.toolbar.addAction(self.action_save)
 
         # ajout d'un bouton réinitialiser
-        spacer = QWidget()
-        spacer.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred) #Permet de modifier l'espace créer pour que l'icone soit à droite
-        self.toolbar.addWidget(spacer)
+        self.toolbar.addSeparator() 
         self.icon2 = QAction('Réinitialiser', self)
         self.toolbar.addAction(self.icon2)
         
+        # Choix des magasins avec un ComboBox
+        self.magasin = QComboBox()
+        self.magasin.addItem("Choix du magasin")
+        self.magasin.addItem("Magasin 1")
+        self.magasin.addItem("Magasin 2")
+        self.magasin.addItem("Magasin 3")
+        self.magasin.setFixedWidth(self.width() // 3)
+        self.menu_selection.addWidget(self.magasin)
+
+        # Liste de course
+        self.liste = QTextEdit()
+        self.liste.setFixedWidth(self.width() // 3)
+        self.menu_selection.addWidget(self.liste)
         
+        # Bouton pour voir le plan
         
-
-
-
-
+        self.ajout_plan = QPushButton("Voir le plan")
+        self.ajout_plan.setFixedHeight(self.height() // 8)
+        self.ajout_plan.setFont(QFont("Arial", 15))
+        self.menu_selection.addWidget(self.ajout_plan)
 
         
+        # Filtre
+        self.filtre_label = QLabel("Filtre")
+        self.filtre1_layout.addWidget(self.filtre_label)
+
+        # Filtre du type de produit
+        self.filtre1 = QComboBox()
+        self.filtre1.addItem("Type de produit")
+        self.filtre1.addItem("Option 2")
+        self.filtre1.setFixedWidth(self.width() // 4)  # Définit la largeur du menu déroulant à 1/4 de la largeur de la fenêtre
+        self.filtre2_layout.addWidget(self.filtre1)
+
+        # Filtre du rayon
+        self.filtre2 = QComboBox()
+        self.filtre2.addItem("Rayon")
+        self.filtre2.addItem("Option B")
+        self.filtre2.setFixedWidth(self.width() // 4)
+        self.filtre2_layout.addWidget(self.filtre2)
+        
+        spacer = QWidget()
+        spacer.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred) #Permet de modifier l'espace créer pour que l'icone soit à droite
+        self.toolbar.addWidget(spacer)
+        self.filtre_label1 = QLabel(" ")
+        self.filtre2_layout.addWidget(self.filtre_label1)
+        self.filtre1_layout.addLayout(self.filtre2_layout)
+        
+        image_label = QLabel(self)
+        pixmap = QPixmap("image/plan1.jpg")
+        image_label.setPixmap(pixmap)
+        self.filtre1_layout.addWidget(image_label)
+
+
+
+
+
+
+
 ##########################################################
 #                                                        #
 #                          Show                          #
@@ -102,6 +131,9 @@ class vueClient(QMainWindow):
         self.showMaximized() # Permet de mettre en ecran total
 
         
+        
+        
+
         
 
 ## Programme principal : test de la vue ---------------------------------------
