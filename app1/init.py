37d1a004576029934e10f63de5a651e9e3464690
tarzanhr <leny621000@gmail.com>
2024-05-07 12:11:31 +0200

    seance 3 - Leny

---
+++ app1/init.py
@@ -0,0 +1,58 @@
+import sys
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+
+class Init(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        # Boutons
+        self.nouveau = QPushButton('Nouveau Projet')
+        self.ouvrir = QPushButton('Ouvrir')
+
+        # Layout pour les boutons
+        layout_boutons = QHBoxLayout()
+        layout_boutons.addWidget(self.nouveau)
+        layout_boutons.addWidget(self.ouvrir)
+        layout_boutons.setContentsMargins(0, 0, 0, 0)
+
+        # Widget pour les boutons
+        boutons_widget = QWidget()
+        boutons_widget.setLayout(layout_boutons)
+
+        # Layout principal
+        layout_principal = QVBoxLayout()
+        layout_principal.addWidget(boutons_widget, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout_principal.setContentsMargins(0, 0, 0, 0)
+
+        # Widget central
+        central_widget = QWidget()
+        central_widget.setLayout(layout_principal)
+        self.setCentralWidget(central_widget)
+
+        # Affichage
+        self.setWindowTitle('INTER-HYPER-MARKET')
+        self.showMaximized()
+        self.show()
+        
+        # Action
+        self.nouveau.clicked.connect(self.lancer)
+        self.ouvrir.clicked.connect(self.lancer)
+        
+        
+        
+        
+    def lancer(self):
+        from vueApp1 import MainWindow
+        self.main_window = MainWindow()
+        self.main_window.show()
+        
+        # Fermer l'interface une fois que la vue est laancer
+        if self.lancer:
+            self.close()
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Init()
+    sys.exit(app.exec())
--- app1/vueApp1.py
+++ app1/vueApp1.py
@@ -1,2 +1,68 @@
 import sys
 from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        # Barre d'outils
+        self.toolbar = QToolBar('')
+        self.addToolBar(Qt.ToolBarArea.TopToolBarArea, self.toolbar)
+
+        # Ajouter des actions à la barre d'outils
+        self.action_nouveau = QAction('Nouveau', self)
+        self.toolbar.addAction(self.action_nouveau)
+
+        self.action_ouvrir = QAction('Ouvrir', self)
+        self.toolbar.addAction(self.action_ouvrir)
+
+        self.action_enregistrer = QAction('Enregistrer', self)
+        self.toolbar.addAction(self.action_enregistrer)
+
+        self.action_supprimer = QAction('Supprimer', self)
+        self.toolbar.addAction(self.action_supprimer)
+
+        # Ajouter un espace extensible à gauche de la barre d'outils pour aligner les boutons à gauche
+        spacer_left = QWidget()
+        spacer_left.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+        self.toolbar.addWidget(spacer_left)
+
+        # Ajouter un espace extensible à droite de la barre d'outils pour aligner le bouton à droite
+        spacer_right = QWidget()
+        spacer_right.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+        self.toolbar.addWidget(spacer_right)
+
+        self.action_reset = QAction('Réinitialiser', self)
+        self.toolbar.addAction(self.action_reset)
+        
+        # layout 
+        
+        self.layout_left : QVBoxLayout = QVBoxLayout()
+        self.lab1 : QLabel = QLabel('Widget gauche')
+        self.layout_left.addWidget(self.lab1)
+        
+        self.layout_right : QHBoxLayout = QHBoxLayout()
+        self.lab2 : QLabel = QLabel('Widget droit')
+        self.layout_right.addWidget(self.lab2) 
+
+        self.layout_groupe : QHBoxLayout = QHBoxLayout()
+
+        # Ajout des layouts à layout_groupe
+        self.layout_groupe.addLayout(self.layout_left)
+        self.layout_groupe.addLayout(self.layout_right)
+        
+        self.widget_centre = QWidget()
+        self.widget_centre.setLayout(self.layout_groupe)
+        self.setCentralWidget(self.widget_centre)
+
+        # Paramètres d'affichage
+        self.setWindowTitle('INTER-HYPER-MARKET')
+        self.showMaximized()
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
