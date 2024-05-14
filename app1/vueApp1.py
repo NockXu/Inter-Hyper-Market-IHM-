68728b118e59820073efa3d56b0438e24ebc90de
tarzanhr <leny621000@gmail.com>
2024-05-14 15:22:37 +0200

    fin seance 6 - leny

--- app1/vueApp1.py
+++ app1/vueApp1.py
@@ -1,107 +1,61 @@
 import sys
+import vueDockMenuOutil
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        # style d'affichage
+        super().__init__()
+        
         self.app = QApplication(sys.argv)
         fichier_style = open(sys.path[0] + "/qss/style.qss", 'r')
         with fichier_style:
             qss = fichier_style.read()
             self.app.setStyleSheet(qss)
 
-        super().__init__()
+        # Barre de menus
+        self.menu_bar = self.menuBar()
 
-        #--------------------------------------------------------------------------------
-
-        # Barre d'outils
-        self.toolbar = QToolBar('')
-        self.addToolBar(Qt.ToolBarArea.TopToolBarArea, self.toolbar)
-
-        #--------------------------------------------------------------------------------
-
-        # Ajouter des actions à la barre d'outils
+        # Menu Fichier
+        file_menu = self.menu_bar.addMenu('Fichier')
         self.action_nouveau = QAction('Nouveau', self)
-        self.toolbar.addAction(self.action_nouveau)
+        file_menu.addAction(self.action_nouveau)
 
         self.action_ouvrir = QAction('Ouvrir', self)
-        self.toolbar.addAction(self.action_ouvrir)
+        file_menu.addAction(self.action_ouvrir)
 
         self.action_enregistrer = QAction('Enregistrer', self)
-        self.toolbar.addAction(self.action_enregistrer)
+        file_menu.addAction(self.action_enregistrer)
 
         self.action_supprimer = QAction('Supprimer', self)
-        self.toolbar.addAction(self.action_supprimer)
+        file_menu.addAction(self.action_supprimer)
 
-        #--------------------------------------------------------------------------------
-
-        # Ajouter un espace extensible à gauche de la barre d'outils pour aligner les boutons à gauche
-        spacer_left = QWidget()
-        spacer_left.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
-        self.toolbar.addWidget(spacer_left)
-
-        # Ajouter un espace extensible à droite de la barre d'outils pour aligner le bouton à droite
-        spacer_right = QWidget()
-        spacer_right.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
-        self.toolbar.addWidget(spacer_right)
-
+        # Menu Edition
+        edit_menu = self.menu_bar.addMenu('Edition')
         self.action_reset = QAction('Réinitialiser', self)
-        self.toolbar.addAction(self.action_reset)
+        edit_menu.addAction(self.action_reset)
 
-        #--------------------------------------------------------------------------------
+        # Menu Affichage
+        view_menu = self.menu_bar.addMenu('Affichage')
+        self.action_barre = QAction('Menu', self)
+        view_menu.addAction(self.action_barre)
 
-        self.layout_dock = QVBoxLayout()
+        # Connecter l'action 'Menu' à la méthode toggle_menu_outil
+        self.action_barre.triggered.connect(self.toggle_menu_outil)
 
-        self.load_plan_button = QPushButton('Charger un plan')
-
-        self.nom_projet_label = QLabel("Nom de projet :")
-        self.nom_projet = QLineEdit()
-
-        self.nom_magasin_label = QLabel('Nom du magasin :')
-        self.nom_magasin = QLineEdit()
-
-        self.auteur_label = QLabel('Auteur :')
-        self.auteur = QLineEdit()
-
-        self.date_label = QLabel('Date de création :')
-        self.date = QDateEdit(calendarPopup=True)
-        self.date.setDate(QDate.currentDate())
-
-        self.adresse_label = QLabel('Adresse du magasin :')
-        self.adresse = QLineEdit()
-
-        self.layout_dock.addWidget(self.load_plan_button)
-        self.layout_dock.addWidget(self.nom_projet_label)
-        self.layout_dock.addWidget(self.nom_projet)
-        self.layout_dock.addWidget(self.nom_magasin_label)
-        self.layout_dock.addWidget(self.nom_magasin)
-        self.layout_dock.addWidget(self.auteur_label)
-        self.layout_dock.addWidget(self.auteur)
-        self.layout_dock.addWidget(self.date_label)
-        self.layout_dock.addWidget(self.date)
-        self.layout_dock.addWidget(self.adresse_label)
-        self.layout_dock.addWidget(self.adresse)
-
-        self.layout_dock.addStretch(1)
-
-        self.layout_right = QHBoxLayout()
-
+        # Utiliser QScrollArea pour l'image
+        self.scroll_area = QScrollArea()
         self.plan_label = QLabel()
-        self.layout_right.addWidget(self.plan_label)
+        self.scroll_area.setWidget(self.plan_label)
+        self.scroll_area.setWidgetResizable(True)
 
-        self.load_plan_button.clicked.connect(self.load_plan)
-
-        #--------------------------------------------------------------------------------
-
-        self.outil = QWidget()
-        self.outil.setLayout(self.layout_dock)
+        self.layout_right = QVBoxLayout()
+        self.layout_right.addWidget(self.scroll_area)
 
         #--------------------------------------------------------------------------------
 
         self.layout_groupe = QHBoxLayout()
-
         self.layout_groupe.addLayout(self.layout_right)
 
         self.widget_centre = QWidget()
@@ -111,10 +65,14 @@ class MainWindow(QMainWindow):
         #--------------------------------------------------------------------------------
 
         self.dock1 = QDockWidget("Menu Outil")
-        self.dock1.setWidget(self.outil)
+        self.vueOutil : vueDockMenuOutil = vueDockMenuOutil.VueDockMenuOutil(self)
+        self.dock1.setWidget(self.vueOutil)
         self.dock1.setFixedWidth(300)
 
         self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dock1)
+
+        # Connecter le bouton load_plan_button à la méthode load_plan
+        self.vueOutil.get_load_plan_button().clicked.connect(self.load_plan)
 
         #--------------------------------------------------------------------------------
 
@@ -130,7 +88,13 @@ class MainWindow(QMainWindow):
             # Affiche l'image sélectionnée dans le QLabel du layout droit
             pixmap = QPixmap(file_name)
             self.plan_label.setPixmap(pixmap)
-            self.plan_label.setScaledContents(True)
+            self.plan_label.adjustSize()
+
+    def toggle_menu_outil(self):
+        if self.dock1.isVisible():
+            self.dock1.hide()
+        else:
+            self.dock1.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ app1/vueDockMenuOutil.py
@@ -0,0 +1,43 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+
+class VueDockMenuOutil(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.layout_dock = QVBoxLayout()
+
+        self.load_plan_button = QPushButton('Charger un plan')
+
+        self.nom_projet_label = QLabel("Nom de projet :")
+        self.nom_projet = QLineEdit()
+
+        self.nom_magasin_label = QLabel('Nom du magasin :')
+        self.nom_magasin = QLineEdit()
+
+        self.auteur_label = QLabel('Auteur :')
+        self.auteur = QLineEdit()
+
+        self.date_label = QLabel('Date de création :')
+        self.date = QDateEdit(calendarPopup=True)
+        self.date.setDate(QDate.currentDate())
+
+        self.adresse_label = QLabel('Adresse du magasin :')
+        self.adresse = QLineEdit()
+
+        self.layout_dock.addWidget(self.load_plan_button)
+        self.layout_dock.addWidget(self.nom_projet_label)
+        self.layout_dock.addWidget(self.nom_projet)
+        self.layout_dock.addWidget(self.nom_magasin_label)
+        self.layout_dock.addWidget(self.nom_magasin)
+        self.layout_dock.addWidget(self.auteur_label)
+        self.layout_dock.addWidget(self.auteur)
+        self.layout_dock.addWidget(self.date_label)
+        self.layout_dock.addWidget(self.date)
+        self.layout_dock.addWidget(self.adresse_label)
+        self.layout_dock.addWidget(self.adresse)
+
+        self.layout_dock.addStretch(1)
+        self.setLayout(self.layout_dock)
+        
+    def get_load_plan_button(self):
+        return self.load_plan_button
