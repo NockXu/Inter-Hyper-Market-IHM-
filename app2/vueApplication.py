a51502c293403154d36a279e6c462fc88ead66fb
Aurélien FONTAINE <aurelienfontaine09@gmail.com>
2024-05-20 21:00:50 +0200

    changement du mot "Fichier" par "Option" dans la barre des taches

--- app2/vueApplication.py
+++ app2/vueApplication.py
@@ -1,9 +1,10 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QSizePolicy
-from PyQt6.QtWidgets import QLabel, QLineEdit, QPushButton, QMainWindow, QToolBar, QComboBox
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QSizePolicy, QScrollArea, QFrame
+from PyQt6.QtWidgets import QLabel, QLineEdit, QPushButton, QMainWindow, QToolBar, QComboBox, QFileDialog, QSpacerItem
 from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6.QtGui import QIcon, QAction, QPixmap, QGuiApplication, QFont
 from vueProduit import VueProduit
+from vueProduit import ProduitWidget
 from vuePlan import VuePlan
 
 
@@ -16,8 +17,6 @@ class VueApplication(QMainWindow):
     # constructeur
     def __init__(self, app):
         super().__init__()
-
-        self.app = app
 
         #style d'affichage
         fichier_style = open(sys.path[0] + "/qss/style.qss", 'r')
@@ -41,6 +40,9 @@ class VueApplication(QMainWindow):
         self.menu_selection = QVBoxLayout()
         self.layout_principal.addLayout(self.menu_selection)
         
+        # Layout de la liste de courses
+        self.liste_layout = QVBoxLayout()
+        
 ##########################################################
 #                                                        #
 #                        Wigets                          #
@@ -48,7 +50,8 @@ class VueApplication(QMainWindow):
 ##########################################################
 
         # Appel des vues du plan et des produits
-        self.produitVue = VueProduit()
+        self.produitVue = VueProduit(self)
+        self.produitWidget = ProduitWidget(self)
         self.planVue = VuePlan()
         self.planVue.hide() # Permet de cacher la vue du plan
 
@@ -56,7 +59,7 @@ class VueApplication(QMainWindow):
         self.menu_bar = self.menuBar()
 
         # ajout d'un bouton sauvegarde
-        fichier_menu = self.menu_bar.addMenu('Fichier')
+        fichier_menu = self.menu_bar.addMenu('Options')
         self.action_save = QAction('Enregistrer', self)
         fichier_menu.addAction(self.action_save)
 
@@ -65,19 +68,25 @@ class VueApplication(QMainWindow):
         fichier_menu.addAction(self.action_reset)
         
         # Choix des magasins avec un ComboBox
-        self.magasin = QComboBox()
-        self.magasin.addItem("Choix du magasin")
-        self.magasin.addItem("Magasin 1")
-        self.magasin.addItem("Magasin 2")
-        self.magasin.addItem("Magasin 3")
+        self.magasin = QPushButton("Choix magasin")
         self.magasin.setFixedWidth(self.width() // 3)
         self.menu_selection.addWidget(self.magasin)
 
-        # Liste de course
-        self.liste = QTextEdit()
-        self.liste.setFixedWidth(self.width() // 3)
-        self.menu_selection.addWidget(self.liste)
+        # La liste de course
+        self.scroll_area = QScrollArea()
+        self.scroll_area_widget = QWidget()
+        self.scroll_area_widget.setLayout(self.liste_layout)
+        self.scroll_area.setWidget(self.scroll_area_widget)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setFixedWidth(self.width() // 3)
+        self.menu_selection.addWidget(self.scroll_area)
         
+        self.spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        self.liste_layout.addItem(self.spacer)
+        
+        self.supp = QPushButton("Supprimer la liste")
+        self.supp.setFont(QFont("Arial", 12))
+        self.menu_selection.addWidget(self.supp)
         # Bouton pour voir le plan
         self.ajout_plan = QPushButton("Voir le plan")
         self.ajout_plan.setFixedHeight(self.height() // 8)
@@ -87,12 +96,28 @@ class VueApplication(QMainWindow):
         self.layout_principal.addWidget(self.produitVue)
         self.layout_principal.addWidget(self.planVue)
         
-        # Connecter le clic sur le bouton "Voir le plan" à une fonction
-        self.ajout_plan.clicked.connect(self.changer_vue)
-
         # Affichage de l'application
         self.setWindowTitle('Application client')
         self.showMaximized() # Permet de mettre en ecran total
+
+##########################################################
+#                                                        #
+#                        Signaux                         #
+#                                                        #
+##########################################################
+
+        self.ajout_plan.clicked.connect(self.changer_vue)
+        self.produitWidget.produit_ajoute.connect(self.ajouter_produit_liste)
+        self.magasin.clicked.connect(self.ouvrir_fichier)
+        self.supp.clicked.connect(self.vider_liste)
+        self.action_reset.triggered.connect(self.reset_application)
+
+
+##########################################################
+#                                                        #
+#                       Fonctions                        #
+#                                                        #
+##########################################################
 
     def changer_vue(self):
         if self.produitVue.isVisible():
@@ -103,6 +128,78 @@ class VueApplication(QMainWindow):
             self.produitVue.show()
             self.planVue.hide()
             self.ajout_plan.setText("Voir le plan")
+            
+    def ajouter_produit_liste(self, nom_produit):
+        # Layout du produit
+        layout = QHBoxLayout()
+        
+        label = QLabel(nom_produit)
+        button = QPushButton("X")
+        button.setFixedWidth(30)
+        button.setFixedHeight(30)
+        
+        button.clicked.connect(lambda: self.retirer_produit_liste(layout, ligne_separation))
+        
+        layout.addWidget(label)
+        layout.addWidget(button)
+        
+        # Ligne separatrice
+        ligne_separation = QFrame()
+        ligne_separation.setFrameShape(QFrame.Shape.HLine)
+        ligne_separation.setFrameShadow(QFrame.Shadow.Sunken)
+        
+        # Insert the product layout and separator before the spacer
+        if self.liste_layout.count() > 1:  # Si il sagit du promier produit dans la liste, on n'ajoute pas de ligne separatrice
+            # Ligne separatrice
+            ligne_separation = QFrame()
+            ligne_separation.setFrameShape(QFrame.Shape.HLine)
+            ligne_separation.setFrameShadow(QFrame.Shadow.Sunken)
+            self.liste_layout.insertWidget(self.liste_layout.count() - 1, ligne_separation)
+        
+        
+        self.liste_layout.insertLayout(self.liste_layout.count() - 1, layout)
+        
+        
+        print("produit ajouté : " + nom_produit)
+
+        
+    def retirer_produit_liste(self, layout, ligne_separation):
+        for i in reversed(range(layout.count())):
+            widget = layout.itemAt(i).widget()
+            if widget:
+                widget.setParent(None)
+        self.liste_layout.removeItem(layout)
+        layout.deleteLater()
+        
+        ligne_separation.setParent(None)
+        
+    # Fonction qui supprime la liste des produits
+    def vider_liste(self):
+        while self.liste_layout.count() > 1:
+            item = self.liste_layout.itemAt(0)
+            if isinstance(item, QHBoxLayout):
+                self.retirer_produit_liste(item, item.itemAt(1).widget())
+            elif isinstance(item.widget(), QFrame):
+                item.widget().setParent(None)
+            else:
+                self.liste_layout.removeItem(item)
+                item.widget().deleteLater()
+        
+    def ouvrir_fichier(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Ouvrir le fichier", "", "All Files (*);;Text Files (*.txt)")
+        if fileName:
+            print("Fichier sélectionné:", fileName)
+            self.vider_liste()
+            self.produitVue.charger_produits(fileName)
+            self.produitVue.filtre1.setCurrentIndex(0)
+            self.produitVue.filtre2.setCurrentIndex(0)
+            
+            
+    def reset_application(self):
+        self.vider_liste()
+        self.produitVue.reset_vue()
+            
+    
 
 ## Programme principal : test de la vue ---------------------------------------
 if __name__ == "__main__":
