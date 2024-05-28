c044e193eda13f73245a73b3c746d89e5d258f62
tarzanhr <leny621000@gmail.com>
2024-05-28 18:01:04 +0200

    fin de seance 14 - leny

--- app1/imageDeplacement.py
+++ app1/imageDeplacement.py
@@ -13,20 +13,28 @@ class ImageDeplacement(QLabel):
         self.rects = []  # Liste pour stocker les rectangles
         self.rows = 0
         self.cols = 0
-        self.selected_cells = set()
+        self.selected_cells = set()  
         self.colored_rects = set()  # Ensemble pour stocker les rectangles colorés
-        self.brush_color = QColor('black')
+        self.brush_color = QColor('black') # Couleur de base du pinceau
     
     # Signaux
     getRectsDeclenchee = pyqtSignal(list)
 
     def set_grid(self, rows, cols):
+        """
+        Définit la grille avec le nombre de lignes et de colonnes.
+
+        Met à jour les propriétés rows et cols et appelle les méthodes
+        pour mettre à jour la grille et recalculer les rectangles.
+
+        Args:
+            rows (int): Nombre de lignes.
+            cols (int): Nombre de colonnes.
+        """
         self.rows = rows
         self.cols = cols
         self.update_grid() 
         self.get_rects()
-
-    
 
     def update_grid(self):
         for frame in self.findChildren(QFrame):
@@ -36,19 +44,19 @@ class ImageDeplacement(QLabel):
         self.colored_rects.clear()  # Nettoyer la liste des rectangles colorés
 
         if self.rows > 0 and self.cols > 0:
-            cell_width = self.width() / self.cols
-            cell_height = self.height() / self.rows
+            cellule_width = self.width() / self.cols
+            cellule_height = self.height() / self.rows
 
             for row in range(self.rows):
                 for col in range(self.cols):
                     frame = QFrame(self)
-                    frame.setGeometry(int(col * cell_width), int(row * cell_height), int(cell_width), int(cell_height))
+                    frame.setGeometry(int(col * cellule_width), int(row * cellule_height), int(cellule_width), int(cellule_height))
                     frame.setFrameShape(QFrame.Shape.Box)
                     frame.setStyleSheet("background-color: transparent; border: 1px solid blue;")
                     frame.setObjectName(f"frame_{row}_{col}")
 
                     # Créer un rectangle pour chaque cellule
-                    rect = QRectF(col * cell_width, row * cell_height, cell_width, cell_height)
+                    rect = QRectF(col * cellule_width, row * cellule_height, cellule_width, cellule_height)
 
                     # Ajouter le rectangle à la liste
                     self.rects.append(rect)
@@ -73,39 +81,56 @@ class ImageDeplacement(QLabel):
                 painter.drawPixmap(self.rect(), self.pixmap())
 
                 if self.rows > 0 and self.cols > 0:
-                    cell_width = self.width() / self.cols
-                    cell_height = self.height() / self.rows
+                    cellule_width = self.width() / self.cols
+                    cellule_height = self.height() / self.rows
 
                     pen = QPen(QColor('blue'))
                     pen.setWidth(1)
                     painter.setPen(pen)
 
                     for row in range(self.rows + 1):
-                        y = int(row * cell_height)
+                        y = int(row * cellule_height)
                         painter.drawLine(0, y, self.width(), y)
 
                     for col in range(self.cols + 1):
-                        x = int(col * cell_width)
+                        x = int(col * cellule_width)
                         painter.drawLine(x, 0, x, self.height())
 
                     for i in self.colored_rects:
                         painter.fillRect(self.rects[i], self.brush_color)
 
     def get_rects(self):
+        """
+        Émet un signal avec les rectangles actuels.
+        """
         self.getRectsDeclenchee.emit(self.rects)
 
     def updateRect(self, index, new_rect):
+        """
+        Met à jour un rectangle à l'index spécifié.
+
+        Args:
+            index (int): L'index du rectangle à mettre à jour.
+            new_rect (QRect): Le nouveau rectangle.
+        """
+        
         self.rects[index] = new_rect
         self.update()
 
     def clearAll(self):
+        """
+        Supprime tout ce qui se trouve sur l'affichage
+        """
+
         self.rows = 0
         self.cols = 0
         self.selected_cells = set()
         self.update_grid()
 
     def set_brush_color(self, color):
-        """Met à jour la couleur de la brosse."""
+        """
+        Met à jour la couleur de la brosse.
+        """
         
         self.brush_color = color
         self.update()
