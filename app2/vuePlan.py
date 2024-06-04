3532ba9cf07faf04d18825414fa382a4119aa8d0
Aurélien FONTAINE <147625120+Aurelienxx@users.noreply.github.com>
2024-06-04 10:58:16 +0200

    modification de l'affichage du plan

--- app2/vuePlan.py
+++ app2/vuePlan.py
@@ -1,8 +1,8 @@
 import sys, os
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QSizePolicy, QFrame
-from PyQt6.QtWidgets import QLabel, QLineEdit, QPushButton, QMainWindow, QToolBar, QComboBox, QScrollArea, QSpacerItem
-from PyQt6.QtCore import Qt, pyqtSignal
-from PyQt6.QtGui import QIcon, QAction, QPixmap, QGuiApplication, QFont
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QGridLayout
+from PyQt6.QtWidgets import QLabel
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from Classes import *
@@ -12,13 +12,38 @@ class VuePlan(QWidget):
     def __init__(self):
         super().__init__()
         
-        
-        self.layout = QVBoxLayout()
+##########################################################
+#                                                        #
+#                   Widgets / Layout                     #
+#                                                        #
+##########################################################
+
+        self.layout = QVBoxLayout(self)
         self.setLayout(self.layout)
-        self.label = QLabel('Plan du magasin')
-        self.layout.addWidget(self.label)
-
-
+        self.plan = QLabel()
+        self.plan.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(self.plan)
+        
+##########################################################
+#                                                        #
+#                      Fonctions                         #
+#                                                        #
+########################################################## 
+    
+    # Fonction qui affiche l'image du plan du magasin    
+    def afficher_plan(self):
+        image = QPixmap('app2/image/plan9.jpg')
+        scaled_image = image.scaled(self.size(), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+        self.plan.setPixmap(scaled_image)
+        self.plan.setScaledContents(True)
+     
+    # Fonction qui efface ce qui se trouve dans plan   
+    def supprimer_plan(self):
+        if not self.plan.pixmap().isNull():
+            self.plan.clear()
+        
+    def afficher_chemin(self, chemin :list[list[Point]]):
+        pass
 
 
 if __name__ == "__main__":
