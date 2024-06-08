9f0b41a1d1f25f3d5e43813e115e6823d8579301
Aurelienxx <aurelienfontaine09@gmail.com>
2024-06-08 17:32:12 +0200

    rectification de l'affichage de l'image

--- app2/vuePlan.py
+++ app2/vuePlan.py
@@ -15,9 +15,21 @@ class VuePlan(QWidget):
         self.largeur = self.width()
         self.longueur = self.height()
 
+    def extraire_chemin(self, chemin_complet, sous_chemin):
+        index = chemin_complet.find(sous_chemin)
+        if index != -1:
+            return chemin_complet[index:]
+        else:
+            return None
+
     def afficher_plan(self, image):
-        self.image = QPixmap("app2/image/" + image)
-        self.update()
+        sous_chemin = "Magasin"
+        chemin_relatif = self.extraire_chemin(image, sous_chemin)
+        if chemin_relatif:
+            self.image = QPixmap(chemin_relatif)
+            self.update()
+        else:
+            print("Sous-chemin non trouvé dans le chemin complet.")
 
     def supprimer_plan(self):
         self.image = None
